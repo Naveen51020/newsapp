4559177f5e14974618984064a0ede2f98db34662
Naveen Kumar <57746456+Naveen51020@users.noreply.github.com>
2022-04-18 10:06:41 +0530

    Project Completion Stage - 1

---
+++ src/components/NewsItem.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span className="badge rounded-pill bg-danger">{source}</span>
+        </div>
+        <img
+          src={
+            imageUrl
+              ? imageUrl
+              : "https://th.bing.com/th/id/OIP.AC9frN1qFnn-I2JCycN8fwHaEK?pid=ImgDet&rs=1"
+          }
+          className="card-img-top"
+          alt="file is Corrupted!!"
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-muted">
+              By {author ? author : "Unknown"} on {new Date(date).toGMTString()}{" "}
+            </small>
+          </p>
+          <a
+            rel="noreferrer"
+            href={newsUrl}
+            target="_blank"
+            className="btn btn-sm btn-dark"
+          >
+            Read More
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsItem;
